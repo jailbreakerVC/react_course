211c519c29ac64685d061879369e5140b1197c65
VIJIT CHANDNA <vijit123chandna@gmail.com>
2022-04-18 16:37:13 +0530

    please work

---
+++ src/components/counter.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Counter extends Component {
+    render() {
+        return (
+            <div>
+                count
+            </div>
+        )
+    }
+}
+
+export default Counter
