f5cd5746aa228b73995893987906bca151dc5ccc
VIJIT CHANDNA <vijit123chandna@gmail.com>
2022-04-18 13:24:03 +0530

    Change of state on button pressed

--- src/components/message.js
+++ src/components/message.js
@@ -7,9 +7,21 @@ class Message extends Component {
                message: 'Welcome Visitor'
           }
      }
+     changeMessage() {
+          this.setState({
+             message:  'thank you for subbing'
+          })
+     }
      render() {
-          return <h1> {this.state.message} </h1>
+          return (
+               <div>
+                    <h1> {this.state.message} </h1>
+                    <button onClick={()=> this.changeMessage()}>subscribe</button>
+                    
+               </div>
+          )
      }
 }
 
 export default Message
+
