a5a89cd26e8e9382502530e4bbeae4d8f1ec4c72
jailbreakerVC <54243183+jailbreakerVC@users.noreply.github.com>
2022-04-21 20:51:52 +0530

    change state

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,13 +1,29 @@
-import { Component } from "react";
+import React, { Component } from 'react'
 
 class Counter extends Component {
-    render() {
-        return (
-            <div>
-                count
-            </div>
-        )
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         count : 0
+      }
     }
+    increment () {
+        this.setState({
+            count: this.state.count +1
+        })
+        console.log(this.state.count)
+
+    }
+    
+  render() {
+    return(<div>
+         <div>Count - {this.state.count}</div>
+         <button onClick={() => this.increment()}>increment</button>
+    </div>)
+    
+  }
 }
 
 export default Counter
